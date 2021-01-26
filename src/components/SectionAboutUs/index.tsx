f6b28a9ac7bf14877e0a6468a9a2dbe8c2bbb727
Hugo Marcelo <hugo.silva@arquivei.com.br>
2021-01-26 11:25:54 -0300

    feat: first commit

---
+++ src/components/SectionAboutUs/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import Heading from 'components/Heading'
+import Container from 'components/Container'
+import ProfileCard from 'components/ProfileCard'
+
+import * as S from './styles'
+import { SectionAboutUsProps } from 'types/api'
+
+const SectionAboutUs = ({ title, authors }: SectionAboutUsProps) => (
+  <Container>
+    <Heading reverseColor>{title}</Heading>
+
+    <S.Content>
+      {authors.map(({ name, photo, role, socialLinks, description }) => (
+        <ProfileCard
+          key={name}
+          name={name}
+          role={role}
+          photo={photo}
+          socialLinks={socialLinks}
+          description={description}
+        />
+      ))}
+    </S.Content>
+  </Container>
+)
+
+export default SectionAboutUs
